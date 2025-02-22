af038a89013f55c99795cee10ab732c04d84cc6d
Pranav Harak <harakpranav@gmail.com>
2025-02-22 18:19:33 +0530

    Update Find Duplicate.cpp

--- Find Duplicate.cpp
+++ Find Duplicate.cpp
@@ -1,16 +1,15 @@
 class Solution {
-    public:
-        int findDuplicate(vector<int>& nums) {
-            int s=nums.size();
-            int res;
-            sort(nums.begin(),nums.end());
-            for(int i=0;i<s;i++){
-                for(int j=i+1;j<s;j++){
-                    if(nums[i]==nums[j]){
-                        return nums[i];
-                    }
+public:
+    int findDuplicate(vector<int>& nums) {
+        int s=nums.size();
+        int res;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<s;i++){
+                if(nums[i]==nums[i+1])
+                {
+                    return nums[i];
                 }
-            }
-        return 0;
         }
-    };
+    return 0;
+    }
+};
