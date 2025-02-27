a536a0efad906afd34c84b60fd776d7995db06de
Pranav Harak <harakpranav@gmail.com>
2025-02-27 19:29:47 +0530

    Update Find First Palindromic String in the Array.cpp

--- Find First Palindromic String in the Array.cpp
+++ Find First Palindromic String in the Array.cpp
@@ -1,14 +1,15 @@
 class Solution {
-    public:
-        vector<int> getConcatenation(vector<int>& nums) {
-            vector<int> add;
-            int s=nums.size();
-            for(int i=0;i<s;i++){
-                add.push_back(nums[i]);
+public:
+    string firstPalindrome(vector<string>& words) {
+        int s=words.size();
+        string str,str1;
+        for(int i=0;i<s;i++){
+            str=words[i];
+            str1=string(str.rbegin(), str.rend());
+            if(str==str1){
+                return str;
             }
-            for(int i=0;i<s;i++){
-                add.push_back(nums[i]);
-            }
-            return add;
         }
-    };
+        return "";
+    }
+};
